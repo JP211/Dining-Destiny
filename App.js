496a253c3f9a62563cb95629bb7f6b0418266a29
Jessica Cockerell <jlpeters211@gmail.com>
2023-09-18 17:54:42 -0500

    Testing Flexbox

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StatusBar, SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Dining Destiny</Text>
+  <>
+  {/* In marginTop - StatusBar.currentHeight is for Safe area view for Android */}
+  <SafeAreaView style={{ flex:1, marginTop: StatusBar.currentHeight }}> 
+
+    <View style={styles.search}>
+      <Text>search</Text>
     </View>
+    
+    <View style={styles.list}>
+      <Text>list</Text>
+    </View>                          
+
+  </SafeAreaView>
+
+  <ExpoStatusBar style="auto" />
+  </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  },
+  search: {
+    
+    backgroundColor: 'green',
+    padding: 25,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: 'blue',
+    padding: 25,
+    
   },
 });
