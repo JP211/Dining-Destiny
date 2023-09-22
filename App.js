8c12c9d0dfdae935603c487415546999d98147b8
Jessica Cockerell <jlpeters211@gmail.com>
2023-09-21 21:04:35 -0500

    Updating theme

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { StyleSheet, SafeAreaView } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components/native";
 
+import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <RestaurantsScreen />
-        <ExpoStatusBar style="auto" />
+        <ThemeProvider theme={theme}>
+          <RestaurantsScreen />
+          <ExpoStatusBar style="auto" />
+        </ThemeProvider>
       </SafeAreaView>
     </>
   );
