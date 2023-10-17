2998b3ada8fd340354522a0dda1902b6fe270491
Jessica Cockerell <jlpeters211@gmail.com>
2023-10-16 22:30:26 -0500

    implementing map callouts

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -2,9 +2,11 @@ import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
 
-import { Search } from "../components/search.component";
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+
+import { Search } from "../components/search.component";
+import { MapCallout } from "../components/map-callout.component";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -18,6 +20,7 @@ export const MapScreen = () => {
   const [latDelta, setLatDelta] = useState(0);
 
   const { lat, lng, viewport } = location;
+
   useEffect(() => {
     const northeastLat = viewport.northeast.lat;
     const southwestLat = viewport.southwest.lat;
@@ -45,7 +48,11 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            ></MapView.Marker>
+            >
+              <MapView.Callout>
+                <MapCallout restaurant={restaurant} />
+              </MapView.Callout>
+            </MapView.Marker>
           );
         })}
       </Map>
