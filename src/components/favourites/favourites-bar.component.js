97a83a34e90357e908af1624e95d782b7377b30f
Jessica Cockerell <jlpeters211@gmail.com>
2023-10-27 21:18:54 -0500

    Finished favourites bar

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -1,4 +1,42 @@
 import React from "react";
-import { Text } from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import { Text } from "../typography/text.component";
 
-export const FavouritesBar = () => <Text>Favourites Bar</Text>;
+import { Spacer } from "../spacer/spacer.component";
+import { CompactRestaurantInfo } from "../restaurant/compact-restaurant-info.component";
+
+const FavouritesWrapper = styled.View`
+  padding: 10px;
+`;
+
+export const FavouritesBar = ({ favourites, onNavigate }) => {
+  if (!favourites.length) {
+    return null;
+  }
+  return (
+    <FavouritesWrapper>
+      <Spacer variant="right.xlarge">
+        <Text variant="body">Favourites</Text>
+      </Spacer>
+      <ScrollView horizontal showHorizontalScrollIndicator={false}>
+        {favourites.map((restaurant) => {
+          const key = restaurant.name;
+          return (
+            <Spacer key={key} position="left" size="medium">
+              <TouchableOpacity
+                onPress={() =>
+                  onNavigate("RestaurantDetail", {
+                    restaurant,
+                  })
+                }
+              >
+                <CompactRestaurantInfo restaurant={restaurant} />
+              </TouchableOpacity>
+            </Spacer>
+          );
+        })}
+      </ScrollView>
+    </FavouritesWrapper>
+  );
+};
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -8,6 +8,7 @@ import { Search } from "../components/search.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { FavouritesContext } from "../../../services/favourites/favourites.context";
 
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
@@ -29,6 +30,7 @@ const LoadingContainer = styled.View`
 `;
 export const RestaurantsScreen = ({ navigation }) => {
   const { isLoading, restaurants } = useContext(RestaurantsContext);
+  const { favourites } = useContext(FavouritesContext);
   const [isToggled, setIsToggled] = useState(false);
   return (
     <SafeArea>
@@ -41,7 +43,7 @@ export const RestaurantsScreen = ({ navigation }) => {
         isFavouritesToggled={isToggled}
         onFavouritesToggle={() => setIsToggled(!isToggled)}
       />
-      {isToggled && <FavouritesBar />}
+      {isToggled && <FavouritesBar favourites={favourites} onNavigate={navigation.navigate} />}
       <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
