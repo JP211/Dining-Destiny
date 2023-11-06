b80f5275b5e345c1c6be389a9c350a8e9f8e01c6
Jessica Cockerell <jlpeters211@gmail.com>
2023-11-05 20:40:50 -0600

    Set up auth context

--- App.js
+++ App.js
@@ -5,11 +5,11 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { initializeApp } from "firebase/app";
 
-import {
-  getAuth,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
+// import {
+//   getAuth,
+//   onAuthStateChanged,
+//   signInWithEmailAndPassword,
+// } from "firebase/auth";
 
 import {
   useFonts as useOswald,
@@ -23,6 +23,7 @@ import { Navigation } from "./src/infrastructure/navigation";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContext } from "./src/services/authentication/authentication.context";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD8ZeDG4h6eOduizaACt2csXV8lCZ-g7po",
@@ -39,22 +40,22 @@ if (firebase.apps.length === 0) {
   firebase.app();
 }
 
-const auth = firebase.auth();
+// const auth = firebase.auth();
 
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  useEffect(() => {
-    setTimeout(() => {
-      signInWithEmailAndPassword(auth, "test@testmail.com", "1234Test")
-        .then((user) => {
-          setIsAuthenticated(true);
-        })
-        .catch((e) => {
-          console.error(e);
-        });
-    }, 5000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     signInWithEmailAndPassword(auth, "test@testmail.com", "1234Test")
+  //       .then((user) => {
+  //         setIsAuthenticated(true);
+  //       })
+  //       .catch((e) => {
+  //         console.error(e);
+  //       });
+  //   }, 5000);
+  // }, []);
 
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -68,18 +69,20 @@ export default function App() {
     return null;
   }
 
-  if (!isAuthenticated) return null;
+  // if (!isAuthenticated) return null;
 
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContext>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthenticationContext>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
